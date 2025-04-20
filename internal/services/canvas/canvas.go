75144524d9d98ce29fe1c8e335e6c5caa999c800
reElcy <reElcy@yandex.ru>
2025-04-20 14:25:03 +0300

    Added: minio storage

--- internal/services/canvas/canvas.go
+++ internal/services/canvas/canvas.go
@@ -12,6 +12,12 @@ const (
 	CanvasPrivacyFriends = "friends" // холст доступен только друзьям
 )
 
+type minioStorage interface {
+	SaveCanvas(ctx context.Context, canvasID string, imageData []byte) error
+	GetCanvas(ctx context.Context, canvasID string) ([]byte, error)
+	DeleteCanvas(ctx context.Context, canvasID string) error
+}
+
 type CanvasService struct {
 }
 
@@ -34,7 +40,7 @@ func (c *CanvasService) GetCanvases(ctx context.Context, canvasIDs []string) ([]
 func (c *CanvasService) UploadImage(ctx context.Context, canvasID string, image []byte) (string, error) {
 	return "", nil
 }
- 
+
 func (c *CanvasService) JoinToCanvas(ctx context.Context, canvasID string, userID string) (string, error) {
 	return "", nil
 }
--- internal/storage/minio_storage/minio.go
+++ internal/storage/minio_storage/minio.go
@@ -1,1 +1,53 @@
 package miniostorage
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/minio/minio-go/v7"
+)
+
+type MinioStorage struct {
+	minioClient *minio.Client
+	bucketName  string
+}
+
+func New(minioClient *minio.Client, bucketName string) *MinioStorage {
+	return &MinioStorage{
+		minioClient: minioClient,
+		bucketName:  bucketName,
+	}
+}
+
+// SaveCanvas сохраняет canvas в MinIO
+func (s *MinioStorage) SaveCanvas(ctx context.Context, canvasID string, imageData []byte) error {
+	objectName := fmt.Sprintf("%s.png", canvasID)
+	_, err := s.minioClient.PutObject(ctx, s.bucketName, objectName, bytes.NewReader(imageData), int64(len(imageData)), minio.PutObjectOptions{
+		ContentType: "image/png",
+	})
+	return err
+}
+
+// GetCanvas получает canvas по ID
+func (s *MinioStorage) GetCanvas(ctx context.Context, canvasID string) ([]byte, error) {
+	objectName := fmt.Sprintf("%s.png", canvasID)
+	obj, err := s.minioClient.GetObject(ctx, s.bucketName, objectName, minio.GetObjectOptions{})
+	if err != nil {
+		return nil, err
+	}
+	defer obj.Close()
+
+	data, err := io.ReadAll(obj)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+// DeleteCanvas удаляет canvas по ID
+func (s *MinioStorage) DeleteCanvas(ctx context.Context, canvasID string) error {
+	objectName := fmt.Sprintf("%s.png", canvasID)
+	return s.minioClient.RemoveObject(ctx, s.bucketName, objectName, minio.RemoveObjectOptions{})
+}
