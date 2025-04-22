17ec55c8d22cca88fa63f05f49009c55d3664b25
reElcy <reElcy@yandex.ru>
2025-04-22 19:21:11 +0300

    Update: kafka producer

---
+++ internal/transport/kafka_transport.go
@@ -0,0 +1,44 @@
+package transport
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/DenisBochko/yandex_Canvas/internal/domain/models"
+	"github.com/DenisBochko/yandex_Canvas/pkg/kafka"
+	"github.com/IBM/sarama"
+	"go.uber.org/zap"
+)
+
+type KafkaTransport struct {
+	Producer sarama.SyncProducer
+	Topic    string
+	log      *zap.Logger
+}
+
+func New(log *zap.Logger, producer sarama.SyncProducer, topic string) *KafkaTransport {
+	return &KafkaTransport{
+		Producer: producer,
+		Topic:    topic,
+		log:      log,
+	}
+}
+
+func (k *KafkaTransport) SendVerificationUserMessage(ctx context.Context, message models.AddToWhiteListMessage) error {
+	messageJson, err := json.Marshal(message)
+
+	if err != nil {
+		return fmt.Errorf("failed to marshal message: %w", err)
+	}
+
+	msg := kafka.PrepareMessage(k.Topic, messageJson)
+	partition, offset, err := k.Producer.SendMessage(msg)
+	if err != nil {
+		k.log.Info("failed to send message", zap.Error(err))
+		return fmt.Errorf("failed to send message: %w", err)
+	}
+	k.log.Info("AddToWhiteListMessage sent to kafka", zap.String("topic", k.Topic), zap.Int32("partition", partition), zap.Int64("offset", offset))
+
+	return nil
+}
---
+++ pkg/kafka/kafka.go
@@ -0,0 +1,100 @@
+package kafka
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/IBM/sarama"
+	"go.uber.org/zap"
+)
+
+type KafkaConfig struct {
+	Brokers []string `yaml:"KAFKA_BROKERS" env-required:"true"`
+	Topic   string   `yaml:"KAFKA_TOPIC" env-required:"true"`
+}
+
+func NewSyncProducer(ctx context.Context, log *zap.Logger, cfg KafkaConfig) (sarama.SyncProducer, error) {
+	brokers := cfg.Brokers
+
+	if len(brokers) == 0 {
+		return nil, sarama.ErrBrokerNotFound
+	}
+
+	// Конфигурация Kafka
+	config := sarama.NewConfig()
+	config.Version = sarama.V2_1_0_0 
+	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	config.Producer.Retry.Max = 5
+	config.Producer.Retry.Backoff = 100 * time.Millisecond
+	config.Producer.Return.Successes = true
+	config.Producer.RequiredAcks = sarama.WaitForAll
+
+	// Создаём топик, если его нет
+	if err := createTopic(cfg, config, log); err != nil {
+		log.Error("failed to create topic", zap.Error(err))
+		return nil, err
+	}
+
+	// Создание продюсера
+	producer, err := sarama.NewSyncProducer(brokers, config)
+	if err != nil {
+		log.Error("failed to create kafka producer", zap.Error(err))
+		return nil, err
+	}
+
+	log.Info("Kafka producer created", zap.Strings("brokers", brokers), zap.String("topic", cfg.Topic))
+	return producer, nil
+}
+
+// createTopic создаёт топик, если он не существует
+func createTopic(cfg KafkaConfig, config *sarama.Config, log *zap.Logger) error {
+	admin, err := sarama.NewClusterAdmin(cfg.Brokers, config)
+	if err != nil {
+		return err
+	}
+	defer admin.Close()
+
+	topicDetail := &sarama.TopicDetail{
+		NumPartitions:     5,
+		ReplicationFactor: 3,
+	}
+
+	err = admin.CreateTopic(cfg.Topic, topicDetail, false)
+	if err != nil {
+		if errors.Is(err, sarama.ErrTopicAlreadyExists) {
+			log.Info("topic already exists", zap.String("topic", cfg.Topic))
+			return nil
+		}
+		return err
+	}
+
+	log.Info("topic created", zap.String("topic", cfg.Topic))
+	return nil
+}
+
+func NewAsyncProducer(ctx context.Context, cfg KafkaConfig) (sarama.AsyncProducer, error) {
+	brokers := cfg.Brokers
+
+	if len(brokers) == 0 {
+		return nil, sarama.ErrBrokerNotFound
+	}
+
+	config := sarama.NewConfig()
+	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Return.Successes = true
+	producer, err := sarama.NewAsyncProducer(brokers, config)
+
+	return producer, err
+}
+
+func PrepareMessage(topic string, message []byte) *sarama.ProducerMessage {
+	msg := &sarama.ProducerMessage{
+		Topic:     topic,
+		Partition: -1,
+		Value:     sarama.ByteEncoder(message),
+	}
+
+	return msg
+}
